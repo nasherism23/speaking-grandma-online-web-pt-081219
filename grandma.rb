1fccfcdd62771e6f6eda11ec49dc7dcce99ae2bf
James Nash <nash.jamesm@gmail.com>
2019-08-14 14:09:14 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -6,7 +6,7 @@ def speak_to_grandma (phrase)
   elsif phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
   else
-    return "HUH?! SPEAK UP SONNY!"
+    return "HUH?! SPEAK UP, SONNY!"
   end
 end
 
